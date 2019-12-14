27667acd41675fa2f71e0396f3ea51ae0b0088cf
Jess Rezac <jess.rezac@gmail.com>
2019-12-14 11:07:57 -0600

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+function superbowlWin(record) {
+  let victory = record.find(function (data) {
+    return data.result === "W"
+  })
+  if (victory) {
+    return victory.year
+  }
 }
