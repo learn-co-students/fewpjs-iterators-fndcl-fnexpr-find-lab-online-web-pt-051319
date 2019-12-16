95d2e65d0841c01fd8916630c522e8d11068b773
nbert100 <nbert14@gmail.com>
2019-12-16 01:49:38 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+superbowlWin = (record) => {
+  //find the record
+  let result = record.find(season => season.result === "W")
+  return result ? result.year : result
+
+}
