2a0908d6b49e2f098df1dbe5e4abb11dc894053c
KristinHannah <kristinhannahk@gmail.com>
2019-12-16 22:38:26 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(array) {
+  let winFind = array.find(function (r) { return r['result'] === "W" })
+  if (winFind !== undefined) {
+    return winFind['year']
+  }
+  else {
+    return undefined
+  }
+}
