83f7995a438cb2f6fa01fdf776bb367c14364c0d
ashok997 <ashok997@gmail.com>
2019-12-28 13:37:27 -0600

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const testVar = {}
 
-function testFunc() {
-  return "hi"
+
+function winYear(record) {
+  return record.result === "W";
 }
+
+function superbowlWin(testVar) {
+  if (testVar.find(winYear)) {
+    const win = testVar.find(winYear);
+    return win.year;
+  }
+
+}
+
+
