f6271f01246bf82c26ca016a62c9cc81532ed501
lexus2727 <lexus2424@yahoo.com>
2019-12-28 20:20:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const testVar = {}
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
 
-function testFunc() {
-  return "hi"
+function superbowlWin(record){
+  if (record.find(e => e.result === "W")){
+    return record.find(e => e.result === "W").year
+  }
+    return undefined
 }
