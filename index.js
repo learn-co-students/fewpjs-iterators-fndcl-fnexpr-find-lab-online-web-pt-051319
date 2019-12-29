2d467fc47fbec3e0f56e46e50a2a7f8268cb751e
jasmine1226 <jasmine1226@gmail.com>
2019-12-28 16:52:22 -0800

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+function superbowlWin(records) {
+  if (records.find(record => record.result === "W")) {
+    return records.find(record => record.result === "W").year;
+  }
 }
