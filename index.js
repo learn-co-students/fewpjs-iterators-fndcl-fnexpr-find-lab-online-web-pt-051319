e803d8b46e6c7f4ab05c84011a255118d6464d68
lexus2727 <lexus2424@yahoo.com>
2019-12-29 11:08:56 -0500

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const record = [
-  {year: "2018", result: "L"},
-  {year: "2017", result: "W"},
-  {year: "2016", result: "N/A"}
-  //...
-]
+  {year: '2018', result: 'L'},
+  {year: '2017', result: 'W'},
+  {year: '2016', result: 'N/A'}
+];
 
-function superbowlWin(record){
-  if (record.find(e => e.result === "W")){
-    return record.find(e => e.result === "W").year
+function superbowlWin(r) { 
+  if (r.year === '2015') {
+    return record
   }
+  else if
+
+  
+   (this === null) {
     return undefined
+  }
 }
+ 
+
+console.log(record.find(superbowlWin)); 
+
+
+
